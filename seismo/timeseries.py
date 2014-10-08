7bcc02ba5a54d4753f956081971a19fa2a26fa04
Ewald Zietsman <ewald.zietsman@gmail.com>
2014-10-08 17:07:18 +0200

    tweak to use small enough blocksize in opencl so my 760 don't crash on large inputs

--- seismo/timeseries.py
+++ seismo/timeseries.py
@@ -155,8 +155,11 @@ def periodogram_opencl(t, m, f):
                                  cl.program_build_info.LOG))
         raise
 
+    # not sure if this is the right name
+    blocksize = min((10000,), amps_g.shape)
+
     # call the function and copy the values from the buffer to a numpy array
-    prg.periodogram(queue, amps_g.shape, None,
+    prg.periodogram(queue, blocksize, None,
                     times_g,
                     mags_g,
                     freqs_g,
