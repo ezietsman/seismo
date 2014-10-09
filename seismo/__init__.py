2bb6970053dfd2e2b27b5922846e889e848c5621
Ewald Zietsman <ewald.zietsman@gmail.com>
2014-10-09 17:33:05 +0200

    Fixed import typo

--- seismo/__init__.py
+++ seismo/__init__.py
@@ -1,3 +1,3 @@
-from .timeseries import deeming, fast_deeming, find_peaks
+from .timeseries import deeming, fast_deeming, find_peak
 from .fitting import signal, sinewave
 from .session import Session
