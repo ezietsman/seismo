7e291aa7f2874dd5d0e1e541c3174113ff77650d
Ewald Zietsman <ewald.zietsman@gmail.com>
2014-11-30 18:44:55 +0200

    some cleanup. Think there was tabs and spaces mixed, wouldn't run

--- tests/test_lomb_scargle.py
+++ tests/test_lomb_scargle.py
@@ -3,6 +3,7 @@ import numpy as np
 from seismo import lomb_scargle
 from scipy.signal import lombscargle as sci_lomb_scargle
 
+
 def create_data():
     ''' Creates a fake data set for testing'''
     # un-evenly sampled data
@@ -11,9 +12,10 @@ def create_data():
     y = np.sin(w*x)
     f = np.linspace(0.01, 10, 100)
     # get close to peak w
-    a_correct = np.array([w,w+2e-2, w+1e-2])
+    a_correct = np.array([w, w+2e-2, w+1e-2])
 
     return x, y, f, a_correct
+
 
 def test_scipy_example():
     '''Compares scipy's implementation to OpenCL'''
@@ -25,12 +27,12 @@ def test_scipy_example():
     nin = 1000
     nout = 100000
     # Fraction of points to select
-    frac_points = 0.9 
+    frac_points = 0.9
     r = np.random.rand(nin)
     x = np.linspace(0.01, 10*np.pi, nin)
     x = x[r >= frac_points]
     y = A * np.sin(w*x+phi)
-     # For normalization of the periodogram
+    # For normalization of the periodogram
     normval = x.shape[0]
     f = np.linspace(0.01, 10, nout)
     # get periodogram
@@ -39,17 +41,16 @@ def test_scipy_example():
     assert(np.allclose(pgram_sci, pgram_cl))
 
 
-
 def test_lomb_scargle_scipy():
     '''Test scipy implementation of lomb scagle'''
-     x, y, f, a_correct = create_data()
-     a = sci_lomb_scargle(x, y, f)
-     assert(np.allclose(f[a.argmax()], a_correct,.02,0))
+    x, y, f, a_correct = create_data()
+    a = sci_lomb_scargle(x, y, f)
+    assert(np.allclose(f[a.argmax()], a_correct, 0.02, 0))
 
 
 def test_lomb_scargle_opencl():
     '''Test OpenCL implementation of lomb scagle'''
-     x, y, f, a_correct = create_data()
-     a = lomb_scargle(x, y, f)
-     # a bit of error since unevenly spaced
-     assert(np.allclose(f[a.argmax(), a_correct,.02,0))
+    x, y, f, a_correct = create_data()
+    a = lomb_scargle(x, y, f)
+    # a bit of error since unevenly spaced
+    assert(np.allclose(f[a.argmax()], a_correct, 0.02, 0))
